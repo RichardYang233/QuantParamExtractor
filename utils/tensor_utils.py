aadd6333667a9e648ac26d06cd857e0be0915e5a
YangChunyi <yangchy1024@163.com>
2025-06-23 19:13:16 +0800

    完善tensor处理相关的函数

--- utils/tensor_utils.py
+++ utils/tensor_utils.py
@@ -1,13 +1,18 @@
 import torch
 
+
 def get_tensor_diff_count(tensor1, tensor2):
-    
+    '''
+    获取两个 Tensor 中不相等元素的个数
+    '''
     diff_count = torch.ne(tensor1, tensor2).sum()
 
     return diff_count.item()
 
 def get_tensor_diff_index(tensor1, tensor2):
-
+    '''
+    获取两个 Tensor 中不相等元素的位置
+    '''
     diff_mask = (tensor1 != tensor2)
 
     diff_index = torch.nonzero(diff_mask, as_tuple=False)
